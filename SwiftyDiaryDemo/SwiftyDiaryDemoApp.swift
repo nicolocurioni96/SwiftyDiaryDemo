e1fa5c1e0071066bad13bfafb725a706617180d9
Nicolò Curioni <102799475+nicolocurioni96@users.noreply.github.com>
2025-03-03 23:23:01 +0200

    Initial Commit

---
+++ SwiftyDiaryDemo/SwiftyDiaryDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyDiaryDemoApp.swift
+//  SwiftyDiaryDemo
+//
+//  Created by Nicolò Curioni on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyDiaryDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftyDiaryDemoTests/SwiftyDiaryDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyDiaryDemoTests.swift
+//  SwiftyDiaryDemoTests
+//
+//  Created by Nicolò Curioni on 03/03/25.
+//
+
+import Testing
+@testable import SwiftyDiaryDemo
+
+struct SwiftyDiaryDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
